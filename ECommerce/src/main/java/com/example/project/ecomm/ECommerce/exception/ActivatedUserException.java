62abd2e76d3b274335ca187f1c961ad39c7f8403
Akanksha Kumari <aakanksha021997@gmail.com>
2021-04-09 20:20:54 +0530

    Removed

--- ECommerce/src/main/java/com/example/project/ecomm/ECommerce/exception/ActivatedUserException.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.ecomm.ECommerce.exception;
-
-public class ActivatedUserException extends RuntimeException
-{
-    public ActivatedUserException(String message)
-    {
-        super(message);
-    }
-}
